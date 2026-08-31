a8d560595da108995f0190f8f0b081d345cbe7ba
riteshkumar-dotcom <ritesh.kumar@lightfurygames.com>
2026-08-31 16:26:15 +0530

    fix

--- Template Queries/device_ram_categories.sql
+++ Template Queries/device_ram_categories.sql
@@ -1,8 +1,16 @@
-CREATE TABLE clevertap.tos_viewed_events_table
-WITH (
-    format = 'PARQUET',
-    external_location = 's3://clevertap-prod-export/athena/tos_viewed_events/'
-)
-AS
-SELECT *
-FROM clevertap.tos_viewed_events;
+
+select case 
+            when device_ram is null then 'Not available'
+            when cast(replace(device_ram, 'GB', '') as integer) < 8 then 'Less than 8 GB'
+            when cast(replace(device_ram, 'GB', '') as integer) >= 8 then '8 GB or more'
+            else 'Not available'
+        end as ram_data
+        , cast(event_time as date) as event_date 
+        , count(distinct email) as num_users
+FROM clevertap.tos_viewed_events
+where cast(event_time as date) >= cast('2026-08-10' as date)
+    and ct_app_version = '9.4.0'
+group by 1,2 
+order by 1,2
+;
+
