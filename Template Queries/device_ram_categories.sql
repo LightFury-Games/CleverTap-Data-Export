f4ea9ec8c9159b5fec299805b35f4b0c5f03efff
riteshkumar-dotcom <ritesh.kumar@lightfurygames.com>
2026-09-07 18:43:21 +0530

    Ladder Screen View

--- Template Queries/device_ram_categories.sql
+++ Template Queries/device_ram_categories.sql
@@ -1,15 +1,294 @@
 
+WITH STAN_5th_Sep_Playtest_Users AS (
+    SELECT *
+    FROM (
+        VALUES
+('haritkadian90@gmail.com'),
+('varshithp2709@gmail.com'),
+('mysticastra7@gmail.com'),
+('saswatswaruppriyadarsan@gmail.com'),
+('subhampanico74@gmail.com'),
+('abhayjairath8@gmail.com'),
+('aadeshgurav540@gmail.com'),
+('shivamtodkari5@gmail.com'),
+('darktiger8gaming@gmail.com'),
+('jeorge92673@gmail.com'),
+('shivanshbaliyan2003@gmail.com'),
+('adarshwade5@gmail.com'),
+('shivprakashpandey.2adj@gmail.com'),
+('kartikjeena49@gmail.com'),
+('badaldebnath4736@gmail.com'),
+('harishhhh0828@gmail.com'),
+('swayam27110@gmail.com'),
+('soudipsaha797@gmail.com'),
+('aryannayak000009@gmail.com'),
+('pathakguddu953@gmail.com'),
+('ofcpikachu05@gmail.com'),
+('mandalsujal215@gmail.com'),
+('mahendrakhadke1978@gmail.com'),
+('sharmaaisha85200@gmail.com'),
+('gurjinderv30@gmail.com'),
+('kunwarsingh1013@gmail.com'),
+('rahul.yadav.chinthala@gmail.com'),
+('ratneshprasad2122@gmail.com'),
+('archaudhari237@gmail.com'),
+('arin3969@gmail.com'),
+('omvkadam123@gmail.com'),
+('mr.yadav987654321@gmail.com'),
+('sahilgaud50@gmail.com'),
+('babymona090000@gmail.com'),
+('nileshsorte498@gmail.com'),
+('aamirhussainn27@gmail.com'),
+('asif212327@gmail.com'),
+('sanjusriacharya17061981@gmail.com'),
+('ramakantdhruw14@gmail.com'),
+('abhi789775@gmail.com'),
+('vrongamers@gmail.com'),
+('anuragsingh257833@gmail.com'),
+('relzzy63@gmail.com'),
+('ravik288227@gmail.com'),
+('parkd3274@gmail.com'),
+('anugrahtirkeygg@gmail.com'),
+('sayandebnath2022@gmail.com'),
+('pritamminecraftgamerz@gmail.com'),
+('mohit77813@gmail.com'),
+('ayaaneqbal069@gmail.com'),
+('srutvik488@gmail.com'),
+('davidkithan961@gmail.com'),
+('chakrabarttybaisakhi@gmail.com'),
+('dkp10072@gmail.com'),
+('priya.dastaan@gmail.com'),
+('nkk83840@gmail.com'),
+('maitrikghetiya@gmail.com'),
+('deep.subho09@gmail.com'),
+('lagewando3@gmail.com'),
+('tyrax.gg@gmail.com'),
+('createrharsh69@gmail.com'),
+('prathamjadhao84@gmail.com'),
+('rudraptl55@gmail.com'),
+('subhammehta702@gmail.com'),
+('pratapyash084@gmail.com'),
+('arghya.growth@gmail.com'),
+('ffarmy5001@gmail.com'),
+('kshitij1212a@gmail.com'),
+('leanguy189@gmail.com'),
+('jshikhar2001@gmail.com'),
+('vimladhurve741@gmail.com'),
+('unclecore24@gmail.com'),
+('sauravkumarsingh6444@gmail.com'),
+('losanbiswal7@gmail.com'),
+('anantsoni731@gmail.com'),
+('sumitkumar042006@gmail.com'),
+('aggamer282@gmail.com'),
+('gulshan48504@gmail.com'),
+('naziaktr17@gmail.com'),
+('subhmannukumar@gmail.com'),
+('vikramyada433@gmail.com'),
+('dhanushviratkohli99@gmail.com'),
+('0660316104.jio@gmail.com'),
+('susant77y6u77@gmail.com'),
+('ayushak77430@gmail.com'),
+('hopexspidy@gmail.com'),
+('chandan3692004@gmail.com'),
+('priyanshup2603@gmail.com'),
+('kinhamza909@gmail.com'),
+('v8615727@gmail.com'),
+('gamingleo956@gmail.com'),
+('adi.roychowdhury2004@gmail.com'),
+('rajdn888@gmail.com'),
+('ravisharma36895@gmail.com'),
+('subhankarmuduli080@gmail.com'),
+('yashkumarsinghaz@gmail.com'),
+('aaradhya.shek@gmail.com'),
+('ohiranjha2213@gmail.com'),
+('nishadniraj653@gmail.com'),
+('saop3256@gmail.com'),
+('punitsaini6907@gmail.com'),
+('pushkarsable03@gmail.com'),
+('sampadtalukdar45@gmail.com'),
+('allaymc985@gmail.com'),
+('adityabp4321@gmail.com'),
+('ultimatejod62@gmail.com'),
+('maroofhs04@gmail.com'),
+('834amanraj@gmail.com'),
+('mantshanaaz64@gmail.com'),
+('kartikayastank1@gmail.com'),
+('khushj02@gmail.com'),
+('jeetkoushikyt22@gmail.com'),
+('mayurnikwade9075@gmail.com'),
+('omsanap9130@gmail.com'),
+('amanraj61109@gmail.com'),
+('laraibsiddiqui02051990@gmail.com'),
+('ksuriyaprakash93@gmail.com'),
+('zenixedits27@gmail.com'),
+('alexjoseph80508@gmail.com'),
+('maharanapratap5750@gmail.com'),
+('rajsipul432@gmail.com'),
+('roy406137@gmail.com'),
+('safalgaur914059@gmail.com'),
+('anshu96417@gmail.com'),
+('bajpaisryansh@gmail.com'),
+('vidishsanadhya@gmail.com'),
+('bilalbelavadi123@gmail.com'),
+('keshavrathore013@gmail.com'),
+('fatminayareen@gmail.com'),
+('sammikumar5237@gmail.com'),
+('tarun.dhanxpert@gmail.com'),
+('riju23biswas@gmail.com'),
+('akhiljain0143@gmail.com'),
+('prathameshbapat40@gmail.com'),
+('aryankumarmishra605@gmail.com'),
+('anurudhsinghthakurji@gmail.com'),
+('armanmishra1512@gmail.com'),
+('sns379326@gmail.com'),
+('akashmunda4143@gmail.com'),
+('marathedaksh28@gmail.com'),
+('ayushpatial1@gmail.com'),
+('shivamkumar981198@gmail.com'),
+('vethekarsahil09@gmail.com'),
+('halerutuja@gmail.com'),
+('dharsubhojit710@gmail.com'),
+('harshit8929raj@gmail.com'),
+('arcticadarsh@gmail.com'),
+('jhah02292008@gmail.com'),
+('vikramsinghkitawat007@gmail.com'),
+('ayushorguts@gmail.com'),
+('maliktarique95@gmail.com'),
+('rudraraftaar1512@gmail.com'),
+('alphahyper66@gmail.com'),
+('gaykeshivam8@gmail.com'),
+('nandadeepy@gmail.com'),
+('sshibambauri@gmail.com'),
+('adhiraof9@gmail.com'),
+('swagatranjanbehera4@gmail.com'),
+('rajatduggal04@gmail.com'),
+('rajendra.32232@gmail.com'),
+('rs463258@gmail.com'),
+('meet.rana804@gmail.com'),
+('aaryanjat5757@gmail.com'),
+('mishrasubhankar09@gmail.com'),
+('shubhamrawat63935@gmail.com'),
+('gutharohithreddy@gmail.com'),
+('shaikhzahir7080@gmail.com'),
+('aryankumar9893921@gmail.com'),
+('nentiidur777@gmail.com'),
+('lakshyaguptaji786@gmail.com'),
+('businesswithrakshitdev@gmail.com'),
+('singh.sahil5121@gmail.com'),
+('shubhamsaraswat2005@gmail.com'),
+('shambhusah629@gmail.com'),
+('aadityasingh07192@gmail.com'),
+('skgiri620457@gmail.com'),
+('freefire.eklavya12@gmail.com'),
+('ayushzinder10077@gmail.com'),
+('vishalagarwal2k03@gmail.com'),
+('dhruwkuldeep31@gmail.com'),
+('piyali1990halder@gmail.com'),
+('vanshdivakar4607@gmail.com'),
+('samudram2004@gmail.com'),
+('sanketghosh1326@gmail.com'),
+('arnavanand0754@gmail.com'),
+('inkhorror339@gmail.com'),
+('singhaniaqueen01@gmail.com'),
+('barkhabirua55@gmail.com'),
+('dikshajadhav2506@gmail.com'),
+('dewanganpranjal521@gmail.com'),
+('royadi2006@gmail.com'),
+('hiteshchandravanshi619@gmail.com'),
+('shekharsaini9785@gmail.com'),
+('hraj93321@gmail.com'),
+('vaishnavmajukar4@gmail.com'),
+('s4nch1ttt@gmail.com'),
+('argonex16@gmail.com'),
+('hasnainraza0170@gmail.com'),
+('chauhanshubh45@gmail.com'),
+('souvik070804@gmail.com'),
+('pradyumnkumawat7704@gmail.com'),
+('samayadiforever21@gmail.com'),
+('abhyudayyad2311@gmail.com'),
+('ankushprasadray@gmail.com'),
+('aadimaurya2626@gmail.com'),
+('engineersaksham123@gmail.com'),
+('kuldeepsah488@gmail.com'),
+('chauhandev320@gmail.com'),
+('utsavpaul25@gmail.com'),
+('freaksx54@gmail.com'),
+('ronafide.rana@gmail.com'),
+('unknowlll301@gmail.com'),
+('rpal9580@gmail.com'),
+('deshmukhrishab749@gmail.com'),
+('ddjdjdn06@gmail.com'),
+('kbopmaterials@gmail.com'),
+('ct114827@gmail.com'),
+('aaravsharm0874@gmail.com'),
+('ankitdey2099@gmail.com'),
+('strikergaming6969@gmail.com'),
+('jagadhabhiram66@gmail.com'),
+('superboy69023@gmail.com'),
+('blazefighter2000@gmail.com'),
+('bhaktaparthaprotim@gmail.com'),
+('aniruddhaclassic9@gmail.com'),
+('sadhukhanganaisoma@gmail.com'),
+('soumyosadhukhan10@gmail.com'),
+('aniruddhapoddar5@gmail.com'),
+('liveentertainment09@gmail.com'),
+('narenderpabboju02@gmail.com'),
+('kanhak4397@gmail.com'),
+('varadchaudhari569@gmail.com'),
+('amankothale001@gmail.com')
+    ) AS t(email)
+)
+
+
 select case 
-            when device_ram is null then 'Not available'
-            when cast(replace(device_ram, 'GB', '') as integer) < 8 then 'Less than 8 GB'
-            when cast(replace(device_ram, 'GB', '') as integer) >= 8 then '8 GB or more'
-            else 'Not available'
+            when device_ram in ('3GB', '4GB') then '4 GB to less than 6 GB'
+            when device_ram in ('6GB', '7GB') then '6 GB to less than 8 GB'
+            when device_ram in ('8GB', '11GB', '12GB', '15GB') then '8 GB or more'
+            else 'Others'
         end as ram_data
-        , cast(event_time as date) as event_date 
-        , count(distinct email) as num_users
-FROM clevertap.tos_viewed_events
-where cast(event_time as date) >= cast('2026-08-10' as date)
-    and ct_app_version = '9.4.0'
-group by 1,2 
-order by 1,2
+     , count(1) as num 
+from 
+(
+select email, max(device_ram) as device_ram
+from 
+(
+
+select email, device_ram 
+from ftue_events 
+where event_date = cast('2026-09-05' as date)
+
+union 
+
+select email, device_ram 
+from session_start_events 
+where event_date = cast('2026-09-05' as date)
+
+union 
+
+select email, device_ram 
+from match_start_events
+where event_date = cast('2026-09-05' as date)
+
+union 
+
+select email, device_ram 
+from match_end_events
+where event_date = cast('2026-09-05' as date)
+
+union 
+
+select email, device_ram 
+from pack_open_completed_events
+where event_date = cast('2026-09-05' as date)
+) a 
+join STAN_5th_Sep_Playtest_Users b using(email)
+where email is not null
+    and email <> ''
+    and email not like '%tester%'
+    and email not like '%lightfury%'
+    and device_ram <> ''
+group by 1 
+)
+group by 1 
+order by 1
 ;
