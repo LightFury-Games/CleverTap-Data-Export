bff48ffa3107463b41b4466699cdc4c2738e615f
riteshkumar-dotcom <ritesh.kumar@lightfurygames.com>
2026-09-13 14:34:33 +0530

    UTC to IST conversion

--- Events/store_item_clicked/store_item_clicked_clean.sql
+++ Events/store_item_clicked/store_item_clicked_clean.sql
@@ -4,15 +4,16 @@ SELECT
     -- Event
     eventName AS event_name,
 
-    -- Unix epoch seconds -> timestamp
-    from_unixtime(
-        TRY_CAST(eventTime AS BIGINT)
-    ) AS event_time,
-    
-        CAST(
-        from_unixtime(
-            TRY_CAST(eventTime AS BIGINT)
-        ) AS DATE
+    -- Unix epoch seconds (UTC) -> IST
+    from_unixtime(TRY_CAST(eventTime AS BIGINT))
+        + INTERVAL '5' HOUR
+        + INTERVAL '30' MINUTE AS event_time,
+
+    CAST(
+        from_unixtime(TRY_CAST(eventTime AS BIGINT))
+            + INTERVAL '5' HOUR
+            + INTERVAL '30' MINUTE
+        AS DATE
     ) AS event_date,
 
     -- Event identifiers
